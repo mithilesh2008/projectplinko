645aebcfd741fa1ac9c19f46660753d706155f40
mithilesh2008 <79626687+mithilesh2008@users.noreply.github.com>
2021-02-27 12:33:35 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ var Engine = Matter.Engine,
  
 var particles = [];
 var plinkos = [];
-
+var divisions=[];
 var divisionHeight=300;
 var score =0;
 function setup() {
@@ -13,7 +13,8 @@ function setup() {
   engine = Engine.create();
   world = engine.world;
   ground = new Ground(width/2,height,width,20);
-
+   
+  score = 0;
 
    for (var k = 0; k <=width; k = k + 80) {
      divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
@@ -64,7 +65,7 @@ function draw() {
      
    }
    if(frameCount%60===0){
-     particles.push(new particle(random(width/2-30, width/2+30), 10,10));
+     particles.push(new Particle(random(width/2-30, width/2+30), 10,10));
      score++;
    }
  
@@ -75,5 +76,6 @@ function draw() {
    for (var k = 0; k < divisions.length; k++) {
      
      divisions[k].display();
+     text("Score : "+ score,300,30);
    }
 }
